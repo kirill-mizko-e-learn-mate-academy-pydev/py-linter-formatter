b99726fa91cad1892ee00549ba294b92b4557bb3
kirill.mizko.e.learn.mate.academy.python <kirill.mizko.e.learn.mate.academy@gmail.com>
2026-03-12 19:58:59 +0200

    done

--- app/main.py
+++ app/main.py
@@ -1,13 +1,47 @@
+
+
+clear_dict = {
+    "line_number" : "line",
+    "column_number" : "column",
+    "text" : "message",
+    "code" : "name",
+    "source" : "source"
+}
+
+
+def gets_errors(errors: list, file_path: str) -> list:
+    error = [
+        format_linter_error(error)
+        for error in errors
+        if error.get("filename") == file_path
+    ]
+
+    satus = "failed" if error else "passed"
+    return [error, satus]
+
+
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return {
+        clear_dict.get(err_key) : "flake8"
+        if err_key == "source" else error.get(err_key)
+        for err_key in list(error.keys()) + ["source"]
+        if err_key in clear_dict
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
-    # write your code here
-    pass
+    return {
+        "errors" : gets_errors(file_path=file_path, errors=errors)[0],
+        "path" : file_path,
+        "status" : gets_errors(file_path=file_path, errors=errors)[1]
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    # write your code here
-    pass
+    return [
+        format_single_linter_file(
+            linter_report_key,
+            linter_report.get(linter_report_key)
+        )
+        for linter_report_key in linter_report.keys()
+    ]
